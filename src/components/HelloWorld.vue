d645a52f3250fbcab12136375d2c46ed5b0f159d
Arnaud <nonokeeper@yahoo.fr>
2022-03-09 14:51:45 +0100

    fix2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -64,8 +64,10 @@ console.log('Token IS : ', ISToken.value);
 <template>
   <h1>{{ msg }}</h1>
 
+  <p>{{ ISToken }}</p>
+
   <p>
-    Recommended IDE setup ({{ ISToken }}):
+    Recommended IDE setup :
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
     +
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
